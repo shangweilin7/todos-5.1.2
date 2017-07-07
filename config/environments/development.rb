bcbec7d8aa008e224a2d2479cf4432a97838851d
shangwei <shangweilin7@gmail.com>
2017-07-07 11:58:36 +0800

    setup forego and webpack-dev-server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,8 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+  config.x.webpacker[:dev_server_host] = "http://127.0.0.1:8080"
+
   config.cache_classes = false
 
   # Do not eager load code on boot.
